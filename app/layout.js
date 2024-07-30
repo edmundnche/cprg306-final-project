7fd9c320d48d91b80560aa340845c431e9e519e3
edmundnche <edmundche.nche@edu.sait.ca>
2024-07-30 16:28:23 -0600

    Adds dark mode feature

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,12 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
+import Providers from './Providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'CPMDb clone',
+  title: 'CPMDb',
   description: 'This is a movie database',
 };
 
@@ -13,8 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
