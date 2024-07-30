7db0e18286f1b1767d622418db49dc5695f349a1
edmundnche <edmundche.nche@edu.sait.ca>
2024-07-30 17:20:10 -0600

    Creates About Page

---
+++ app/about/page.jsx
@@ -0,0 +1,25 @@
+export default function About() {
+    return (
+        <div className='max-w-6xl mx-auto p-3 space-y-4'>
+            <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+            <p>
+                Welcome to the CPMDb website! I am passionate movie enthusiast
+                and this is a class project to create a one-stop destination for
+                persons with movie-related needs.
+            </p>
+
+            <p>
+                This website provides a vast collection of movies from around the world.
+                The database is regularly updated with new releases, ensuring you always
+                have access to the latest films. You can search for movie titles using keywords
+                and you can find trending and top-rated movies, making it easy to find
+                exactly what you're looking for.
+            </p>
+
+            <p>
+                Thank you for visiting the website and I hope you
+                enjoy your time browsing through CPMDb. Happy browsing!
+            </p>
+        </div>
+    );
+}
