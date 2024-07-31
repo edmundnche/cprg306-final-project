8f131c296ee991a01845555553602ca0ccc5bf48
edmundnche <edmundche.nche@edu.sait.ca>
2024-07-31 00:51:01 -0600

    Creates Navbar component and fixes build error

--- components/NavbarItem.jsx
+++ components/NavbarItem.jsx
@@ -2,21 +2,30 @@
 
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-export default function NavbarItem({ title, param }) {
+function NavbarContent({ title, param }) {
     const searchParams = useSearchParams();
     const genre = searchParams.get('genre');
     return (
         <div>
             <Link
                 className={`hover:text-amber-600 font-semibold ${genre === param
-                        ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
-                        : ''
+                    ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+                    : ''
                     }`}
                 href={`/?genre=${param}`}
             >
                 {title}
             </Link>
         </div>
+    );
+}
+
+export default function NavbarItem({ title, param }) {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <NavbarContent title={title} param={param} />
+        </Suspense>
     );
 }
