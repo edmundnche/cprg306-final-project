08c3572178ef217321c7ff84b7b6d402cc28b106
edmundnche <edmundche.nche@edu.sait.ca>
2024-08-03 14:57:19 -0600

    Updates home page to try fix vercel deployment issue

--- app/page.js
+++ app/page.js
@@ -4,20 +4,27 @@ const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || 'fetchTrending';
-  const res = await fetch(
-    `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`,
-    { next: { revalidate: 10000 } });
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+      }?api_key=${API_KEY}&language=en-US&page=1`,
+      { next: { revalidate: 10000 } }
+    );
 
-  const data = await res.json();
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    const results = data.results;
+
+    return (
+      <div>
+        <Results results={results} />
+      </div>
+    );
+  } catch (error) {
+    console.error(error);
+    return <div>Failed to load data. Please try again later.</div>;
   }
-  const results = data.results;
-
-  return (
-    <div>
-      <Results results={results} />
-    </div>
-  );
 }
