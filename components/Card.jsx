b378d277d6e1dafb646e0763cb3b73e46381b97f
edmundnche <edmundche.nche@edu.sait.ca>
2024-08-03 16:19:38 -0600

    Makes changes to the card and results components to attempt to fix vercel deployment issues

--- components/Card.jsx
+++ components/Card.jsx
@@ -6,18 +6,26 @@ export default function Card({ result }) {
     return (
         <div className='group cursor-pointer hover:shadow-2xl shadow-lg rounded-md border border-gray-700 m-4 bg-gray-800 transition-shadow duration-300'>
             <Link href={`/movie/${result.id}`}>
-                <Image
-                    src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
-                    alt={result.title || 'Movie Poster'}
-                    width={500}
-                    height={300}
-                    className='rounded-t-md'
-                />
-                <div className='p-2'>
-                    <p className='truncate text-lg font-bold'>{result.title || 'No Title'}</p>
-                    <p className='truncate text-sm'>{result.overview || 'No Overview'}</p>
-                    <p className='flex items-center'>
-                        <FiThumbsUp className='h-5 mr-1' /> {result.vote_count || 0}
+                {result.backdrop_path || result.poster_path ? (
+                    <Image
+                        src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
+                        width={500}
+                        height={300}
+                        className='rounded-t-md group-hover:opacity-85 transition-opacity duration-300'
+                        alt={`${result.title || result.name} poster`}
+                    />
+                ) : (
+                    <div style={{ width: 500, height: 300, backgroundColor: '#555' }} className='rounded-t-md'></div>
+                )}
+                <div className='p-5'>
+                    <p className='line-clamp-3 text-base text-gray-200 font-medium leading-tight'>{result.overview}</p>
+                    <h2 className='text-xl font-semibold text-yellow-400 truncate mt-3'>
+                        {result.title || result.name}
+                    </h2>
+                    <p className='flex items-center text-sm text-gray-400 mt-2'>
+                        {result.release_date || result.first_air_date}
+                        <FiThumbsUp className='h-5 w-5 mr-1 ml-3 text-red-400' />
+                        {result.vote_count}
                     </p>
                 </div>
             </Link>
